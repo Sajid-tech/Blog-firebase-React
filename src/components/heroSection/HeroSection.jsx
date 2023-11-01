c32f739c66d962baf0592a7411c1c4f9f31ef780
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-11-01 16:36:21 +0530

    navbar ,footer done

---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,14 @@
+import { Typography } from "@material-tailwind/react";
+import React from "react";
+
+function HeroSection() {
+  return (
+    <img
+      className="h-96 w-full lg:mt-2 object-cover object-center"
+      src="https://traveljunkies.com/wp-content/uploads/2023/01/adventure1.jpg"
+      alt="nature image"
+    />
+  );
+}
+
+export default HeroSection;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Layout from "../../components/layout/Layout";
+import HeroSection from "../../components/heroSection/HeroSection";
+import BlogPostCard from "../../components/blogPostCard/BlogPostCard";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Layout>
+      <HeroSection />
+      <BlogPostCard />
+    </Layout>
+  );
 };
 
 export default Home;
