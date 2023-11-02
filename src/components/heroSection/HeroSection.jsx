477779e9eec9adbc9f0eedc104fffdea7a014aca
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-11-02 15:30:00 +0530

    hero banner

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -3,11 +3,20 @@ import React from "react";
 
 function HeroSection() {
   return (
-    <img
-      className="h-96 w-full lg:mt-2 object-cover object-center"
-      src="https://traveljunkies.com/wp-content/uploads/2023/01/adventure1.jpg"
-      alt="nature image"
-    />
+    <div className="relative">
+      <div>
+        <img
+          className="w-full h-96 lg:mt-2 object-cover object-center"
+          src="https://traveljunkies.com/wp-content/uploads/2023/01/adventure1.jpg"
+          alt="nature image"
+        />
+      </div>
+      <div>
+        <span className="absolute top-[50%] left-[50%] font-bold text-5xl text-light-green-800 ">
+          hello world
+        </span>
+      </div>
+    </div>
   );
 }
 
