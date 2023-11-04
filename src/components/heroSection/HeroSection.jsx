fe96d538864112fc20627f573515913f2197bdba
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-11-04 11:33:40 +0530

    changed: herosection ui

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -3,19 +3,24 @@ import React from "react";
 
 function HeroSection() {
   return (
-    <div className="relative">
-      <div>
-        <img
-          className="w-full h-96 lg:mt-2 object-cover object-center"
-          src="https://traveljunkies.com/wp-content/uploads/2023/01/adventure1.jpg"
-          alt="nature image"
-        />
-      </div>
-      <div>
-        <span className="absolute top-[50%] left-[50%] font-bold text-5xl text-light-green-800 ">
-          hello world
-        </span>
-      </div>
+    <div className=" relative w-full h-96 object-fill">
+      <img
+        className="h-full w-full  object-cover object-center "
+        src="https://traveljunkies.com/wp-content/uploads/2023/01/adventure1.jpg"
+        alt="nature image"
+      />
+
+      <figcaption className="absolute bottom-8 left-2/4 flex w-[calc(100%-4rem)] -translate-x-2/4 justify-between rounded-xl border border-white bg-white/75 py-4 px-6 shadow-lg shadow-black/5 saturate-200 backdrop-blur-sm">
+        <div>
+          <div color="blue-gray">Sara Lamalo</div>
+          <div color="gray" className="mt-2 font-normal">
+            20 July 2022
+          </div>
+        </div>
+        <div variant="h5" color="blue-gray">
+          Growth
+        </div>
+      </figcaption>
     </div>
   );
 }
